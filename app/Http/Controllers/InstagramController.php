34c78c58b0971549122638767d82eb3ade177ce0
pawruol <pawruol@gmail.com>
2020-08-06 01:01:18 +0300

    write some instructions

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Jobs\InstagramGetAccountFollowersJob;
-use App\Jobs\InstagramGetAccountFollowingsJob;
+use App\Jobs\InstagramGetAccountFollowersJob;
 
 use Illuminate\Support\Facades\Auth;
 
