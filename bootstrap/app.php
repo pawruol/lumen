97e66624b0de2f920cb49ca7389410c34c73bb2e
pawruol <pawruol@gmail.com>
2020-08-10 22:20:48 +0300

    frontend init commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,9 +74,9 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    \App\Http\Middleware\Cors::class,
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
